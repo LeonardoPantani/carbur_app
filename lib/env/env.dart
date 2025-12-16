ab4de4679e0f60d0ab08159eb84e6e38a511d1ef
Leonardo <leonardo.pantani@claranet.com>
2025-12-16 14:32:50 +0100

    preparing ios

--- lib/env/env.dart
+++ lib/env/env.dart
@@ -4,8 +4,8 @@ part 'env.g.dart';
 
 // compilare con: dart run build_runner build
 
-@Envied(path: '.env', useConstantCase: true)
+@Envied(path: '.env')
 abstract class Env {
-  @EnviedField(obfuscate: true)
+  @EnviedField(obfuscate: true, varName: "GOOGLE_MAPS_SDK_API_KEY")
   static final String googleMapsSdkApiKey = _Env.googleMapsSdkApiKey;
 }
